e74ceb12b8f6b066da0972dbba28829a92c0bf5d
Eduardo Lima Mitev <elima@igalia.com>
2009-10-02 11:55:32 +0200

    Adds autotools check for gobject-introspection.
    Also organizes autotools m4 macros into 'build/autotools' folder (as copied from Clutter project :-)

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
-[ -e config.cache ] && rm -f config.cache
 
-libtoolize --automake
-aclocal
-autoconf
-autoheader
-automake -a
-./configure $@
-exit
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
+PROJECT=evd
+TEST_TYPE=-d
+FILE=evd
+
+test $TEST_TYPE $FILE || {
+        echo "You must run this script in the top-level $PROJECT directory"
+        exit 1
+}
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found ***"
+        exit 1
+else
+        ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit $?
+fi
+
+./configure "$@" && echo "Now type 'make' to compile $PROJECT."
