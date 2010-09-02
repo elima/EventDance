ae1d1afe5340e8c2e92c211486fd6aa0f6cb3676
Eduardo Lima Mitev <elima@igalia.com>
2010-09-03 00:34:24 +0200

    Makes EvdTransport a GInterfaceType instead

--- evd/evd-transport.c
+++ evd/evd-transport.c
@@ -3,7 +3,7 @@
  *
  * EventDance project - An event distribution framework (http://eventdance.org)
  *
- * Copyright (C) 2009, Igalia S.L.
+ * Copyright (C) 2009, 2010 Igalia S.L.
  *
  * Authors:
  *   Eduardo Lima Mitev <elima@igalia.com>
@@ -19,36 +19,9 @@
  * for more details.
  */
 
-#include <string.h>
-
 #include "evd-transport.h"
 
-#include "evd-utils.h"
-#include "evd-error.h"
-
-G_DEFINE_ABSTRACT_TYPE (EvdTransport, evd_transport, EVD_TYPE_SERVICE)
-
-#define EVD_TRANSPORT_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
-                                        EVD_TYPE_TRANSPORT, \
-                                        EvdTransportPrivate))
-
-#define DEFAULT_PEER_TIMEOUT_INTERVAL  5 /* seconds */
-#define DEFAULT_PEER_CLEANUP_INTERVAL 10 /* seconds */
-
-/* private data */
-struct _EvdTransportPrivate
-{
-  GHashTable *peers;
-
-  guint peer_timeout_interval;
-
-  GTimer *peer_cleanup_timer;
-  guint peer_cleanup_interval;
-
-  EvdPeer *rcv_peer;
-  gchar *rcv_buf;
-  gsize rcv_size;
-};
+#define PEER_MSG_KEY "org.eventdance.transport.PeerMessage"
 
 /* signals */
 enum
@@ -57,313 +30,124 @@ enum
   SIGNAL_LAST
 };
 
+typedef struct
+{
+  const gchar *buffer;
+  gsize size;
+} EvdTransportPeerMessage;
+
 static guint evd_transport_signals[SIGNAL_LAST] = { 0 };
 
-static void     evd_transport_class_init         (EvdTransportClass *class);
-static void     evd_transport_init               (EvdTransport *self);
-
-static void     evd_transport_finalize           (GObject *obj);
-static void     evd_transport_dispose            (GObject *obj);
-
-static EvdPeer *evd_transport_create_new_peer    (EvdTransport *self);
-
-static void     evd_transport_receive_internal   (EvdTransport *self,
-                                                  EvdPeer      *peer,
-                                                  gchar        *buffer,
-                                                  gsize         size);
+static void  evd_transport_receive_internal (EvdTransport *self,
+                                             EvdPeer       *peer,
+                                             gchar         *buffer,
+                                             gsize          size);
 
 static void
-evd_transport_class_init (EvdTransportClass *class)
+evd_transport_base_init (gpointer g_class)
 {
-  GObjectClass *obj_class = G_OBJECT_CLASS (class);
+  static gboolean is_initialized = FALSE;
 
-  obj_class->dispose = evd_transport_dispose;
-  obj_class->finalize = evd_transport_finalize;
+  EvdTransportInterface *iface = (EvdTransportInterface *) g_class;
 
-  class->create_new_peer = evd_transport_create_new_peer;
-  class->receive = evd_transport_receive_internal;
+  iface->receive = evd_transport_receive_internal;
 
-  evd_transport_signals[SIGNAL_RECEIVE] =
-    g_signal_new ("receive",
-                  G_TYPE_FROM_CLASS (obj_class),
-                  G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
-                  G_STRUCT_OFFSET (EvdTransportClass, signal_receive),
-                  NULL, NULL,
-                  g_cclosure_marshal_VOID__VOID,
-                  G_TYPE_NONE, 0);
-
-  /* add private structure */
-  g_type_class_add_private (obj_class, sizeof (EvdTransportPrivate));
-}
-
-static void
-evd_transport_init (EvdTransport *self)
-{
-  EvdTransportPrivate *priv;
-
-  priv = EVD_TRANSPORT_GET_PRIVATE (self);
-  self->priv = priv;
-
-  priv->peers = g_hash_table_new_full (g_str_hash,
-                                       g_str_equal,
-                                       g_free,
-                                       g_object_unref);
-
-  priv->peer_timeout_interval = DEFAULT_PEER_TIMEOUT_INTERVAL;
-
-  priv->peer_cleanup_timer = g_timer_new ();
-  priv->peer_cleanup_interval = DEFAULT_PEER_CLEANUP_INTERVAL;
-
-  priv->rcv_peer = NULL;
-  priv->rcv_buf = NULL;
-  priv->rcv_size = 0;
-}
-
-static void
-evd_transport_dispose (GObject *obj)
-{
-  G_OBJECT_CLASS (evd_transport_parent_class)->dispose (obj);
-}
-
-static void
-evd_transport_finalize (GObject *obj)
-{
-  EvdTransport *self = EVD_TRANSPORT (obj);
-
-  g_hash_table_destroy (self->priv->peers);
-
-  g_timer_destroy (self->priv->peer_cleanup_timer);
-
-  G_OBJECT_CLASS (evd_transport_parent_class)->finalize (obj);
-}
-
-static void
-evd_transport_destroy_peer (EvdTransport *self,
-                            EvdPeer      *peer)
-{
-  /* @TODO: emit signal to notify the world */
-}
-
-static gboolean
-evd_transport_check_peer (gpointer key,
-                          gpointer value,
-                          gpointer user_data)
-{
-  EvdTransport *self = EVD_TRANSPORT (user_data);
-  EvdPeer *peer = EVD_PEER (value);
-
-  if (evd_transport_peer_is_dead (self, peer))
+  if (!is_initialized)
     {
-      evd_transport_destroy_peer (self, peer);
-      return TRUE;
-    }
-  else
-    {
-      return FALSE;
+      evd_transport_signals[SIGNAL_RECEIVE] =
+        g_signal_new ("receive",
+                      G_TYPE_FROM_CLASS (g_class),
+                      G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
+                      G_STRUCT_OFFSET (EvdTransportInterface, signal_receive),
+                      NULL, NULL,
+                      g_cclosure_marshal_VOID__OBJECT,
+                      G_TYPE_NONE, 1,
+                      EVD_TYPE_PEER);
+
+      is_initialized = TRUE;
     }
 }
 
-static void
-evd_transport_cleanup_peers (EvdTransport *self)
+GType
+evd_transport_get_type (void)
 {
-  if (g_timer_elapsed (self->priv->peer_cleanup_timer, NULL) <=
-      self->priv->peer_cleanup_interval)
-    return;
+  static GType iface_type = 0;
 
-  g_timer_start (self->priv->peer_cleanup_timer);
+  if (iface_type == 0)
+    {
+      static const GTypeInfo info = {
+        sizeof (EvdTransportInterface),
+        evd_transport_base_init,
+        NULL,
+      };
 
-  g_hash_table_foreach_remove (self->priv->peers,
-                               evd_transport_check_peer,
-                               self);
+      iface_type = g_type_register_static (G_TYPE_INTERFACE, "EvdTransport",
+                                           &info, 0);
+    }
+
+  return iface_type;
 }
 
-static EvdPeer *
-evd_transport_create_new_peer (EvdTransport *self)
+static void
+evd_transport_free_peer_msg (gpointer  data,
+                             GObject  *where_the_object_was)
 {
-  EvdPeer *peer;
-  gchar *id;
+  EvdTransportPeerMessage *msg;
 
-  g_return_val_if_fail (EVD_IS_TRANSPORT (self), NULL);
+  msg = g_object_get_data (G_OBJECT (where_the_object_was), PEER_MSG_KEY);
 
-  id = evd_uuid_new ();
-
-  peer = g_object_new (EVD_TYPE_PEER,
-                       "id", id,
-                       "transport", self,
-                       NULL);
-
-  g_hash_table_insert (self->priv->peers, id, peer);
-
-  return peer;
+  if (msg != NULL)
+    g_slice_free (EvdTransportPeerMessage, msg);
 }
 
 static void
 evd_transport_receive_internal (EvdTransport *self,
-                                EvdPeer      *peer,
-                                gchar        *buffer,
-                                gsize         size)
+                                EvdPeer       *peer,
+                                gchar         *buffer,
+                                gsize          size)
 {
-  self->priv->rcv_peer = peer;
-  self->priv->rcv_buf = buffer;
-  self->priv->rcv_size = size;
+  EvdTransportPeerMessage *msg;
 
-  g_signal_emit (self, evd_transport_signals[SIGNAL_RECEIVE], 0, NULL);
+  msg = g_object_get_data (G_OBJECT (peer), PEER_MSG_KEY);
+  if (msg == NULL)
+    {
+      msg = g_slice_new (EvdTransportPeerMessage);
+      g_object_set_data (G_OBJECT (peer), PEER_MSG_KEY, msg);
 
-  self->priv->rcv_peer = NULL;
-  self->priv->rcv_buf = NULL;
-  self->priv->rcv_size = 0;
+      g_object_weak_ref (G_OBJECT (peer), evd_transport_free_peer_msg, NULL);
+    }
+
+  msg->buffer = buffer;
+  msg->size = size;
+
+  g_signal_emit (self, evd_transport_signals[SIGNAL_RECEIVE], 0, peer, NULL);
+
+  msg->buffer = NULL;
+  msg->size = 0;
 
   g_free (buffer);
 }
 
-static gssize
-evd_transport_send (EvdTransport  *self,
+/* public methods */
+
+gboolean
+evd_transport_send (EvdTransport *self,
                     EvdPeer       *peer,
                     const gchar   *buffer,
                     gsize          size,
-                    GError       **error)
+                    GError        **error)
 {
-  EvdTransportClass *class;
-
-  g_return_val_if_fail (EVD_IS_TRANSPORT (self), -1);
-  g_return_val_if_fail (EVD_IS_PEER (peer), -1);
-
-  evd_transport_cleanup_peers (self);
-
-  /* check the peer is one of our peers */
-  if (g_hash_table_lookup (self->priv->peers, evd_peer_get_id (peer)) != peer)
-    {
-      g_set_error_literal (error,
-                           EVD_ERROR,
-                           EVD_ERROR_REFUSED,
-                           "Peer doesn't belong to transport");
-
-      return -1;
-    }
-
-  /* check peer is not dead */
-  if (evd_transport_peer_is_dead (self, peer))
-    {
-      evd_transport_destroy_peer (self, peer);
-
-      g_set_error_literal (error,
-                           EVD_ERROR,
-                           EVD_ERROR_CLOSED,
-                           "Peer has been invalidated due to inactivity");
-
-      return -1;
-    }
-
-  class = EVD_TRANSPORT_GET_CLASS (self);
-
-  /* add frame to peer's backlog if peer cannot send it just right now */
-  if (evd_peer_backlog_get_length (peer) > 0 ||
-      class->peer_is_connected == NULL ||
-      ! class->peer_is_connected (self, peer))
-    {
-      if (! evd_peer_backlog_push_frame (peer, buffer, size, error))
-        return -1;
-      else
-        return size;
-    }
-
-  if (class->send != NULL)
-    {
-      if (! class->send (self, peer, buffer, size, NULL) &&
-          ! evd_peer_backlog_push_frame (peer, buffer, size, error))
-        return -1;
-      else
-        return size;
-    }
-  else
-    {
-      g_set_error_literal (error,
-                           EVD_ERROR,
-                           EVD_ERROR_ABSTRACT,
-                           "Cannot call abstract method 'send'");
-
-      return -1;
-    }
-}
-
-/* public methods */
-
-EvdTransport *
-evd_transport_new (void)
-{
-  EvdTransport *self;
-
-  self = g_object_new (EVD_TYPE_TRANSPORT, NULL);
-
-  return self;
-}
-
-/**
- * evd_transport_lookup_peer:
- *
- * Returns: (transfer none): The #EvdPeer, or NULL if not found.
- **/
-EvdPeer *
-evd_transport_lookup_peer (EvdTransport *self, const gchar *id)
-{
-  EvdPeer *peer;
-
-  g_return_val_if_fail (EVD_IS_TRANSPORT (self), NULL);
-  g_return_val_if_fail (id != NULL, NULL);
-
-  peer = EVD_PEER (g_hash_table_lookup (self->priv->peers,
-                                        (gconstpointer) id));
-
-  if (peer != NULL && evd_transport_peer_is_dead (self, peer))
-    {
-      evd_transport_destroy_peer (self, peer);
-      g_hash_table_remove (self->priv->peers, id);
-
-      peer = NULL;
-    }
-
-  return peer;
-}
-
-/**
- * evd_transport_get_all_peers:
- *
- * Returns: (transfer container) (element-type Evd.Peer):
- **/
-GList *
-evd_transport_get_all_peers (EvdTransport *self)
-{
-  g_return_val_if_fail (EVD_IS_TRANSPORT (self), NULL);
-
-  evd_transport_cleanup_peers (self);
-
-  return g_hash_table_get_values (self->priv->peers);
-}
-
-gboolean
-evd_transport_send_utf8 (EvdTransport  *self,
-                         EvdPeer       *peer,
-                         const gchar   *str,
-                         GError       **error)
-{
-  return evd_transport_send (self, peer, str, strlen (str), error);
-}
-
-gboolean
-evd_transport_peer_is_dead (EvdTransport *self,
-                            EvdPeer      *peer)
-{
-  EvdTransportClass *class;
+  gssize actual_size;
 
   g_return_val_if_fail (EVD_IS_TRANSPORT (self), FALSE);
   g_return_val_if_fail (EVD_IS_PEER (peer), FALSE);
 
-  if (evd_peer_get_idle_time (peer) <= self->priv->peer_timeout_interval)
-    return FALSE;
-
-  class = EVD_TRANSPORT_GET_CLASS (self);
-  if (class->peer_is_connected == NULL ||
-      class->peer_is_connected (self, peer))
+  if ( (actual_size = EVD_TRANSPORT_GET_INTERFACE (self)->send (self,
+                                                                peer,
+                                                                buffer,
+                                                                size,
+                                                                NULL)) <= 0 &&
+       ! evd_peer_backlog_push_frame (peer, buffer, size, error))
     return FALSE;
   else
     return TRUE;
@@ -371,23 +155,35 @@ evd_transport_peer_is_dead (EvdTransport *self,
 
 /**
  * evd_transport_receive:
- * @peer: (out) (transfer none):
  * @size: (out):
  *
  * Returns: (transfer none):
  **/
 const gchar *
-evd_transport_receive (EvdTransport  *self,
-                       EvdPeer      **peer,
+evd_transport_receive (EvdTransport *self,
+                       EvdPeer       *peer,
                        gsize         *size)
 {
+  EvdTransportPeerMessage *msg;
+
   g_return_val_if_fail (EVD_IS_TRANSPORT (self), NULL);
 
-  if (peer != NULL)
-    *peer = self->priv->rcv_peer;
+  msg = g_object_get_data (G_OBJECT (peer), PEER_MSG_KEY);
+  if (msg == NULL)
+    return NULL;
 
   if (size != NULL)
-    *size = self->priv->rcv_size;
+    *size = msg->size;
 
-  return self->priv->rcv_buf;
+  return msg->buffer;
+}
+
+gboolean
+evd_transport_peer_is_connected (EvdTransport *self,
+                                 EvdPeer       *peer)
+{
+  g_return_val_if_fail (EVD_IS_TRANSPORT (self), FALSE);
+  g_return_val_if_fail (EVD_IS_PEER (peer), FALSE);
+
+  return EVD_TRANSPORT_GET_INTERFACE (self)->peer_is_connected (self, peer);
 }
--- evd/evd-transport.h
+++ evd/evd-transport.h
@@ -3,7 +3,7 @@
  *
  * EventDance project - An event distribution framework (http://eventdance.org)
  *
- * Copyright (C) 2009, Igalia S.L.
+ * Copyright (C) 2009, 2010 Igalia S.L.
  *
  * Authors:
  *   Eduardo Lima Mitev <elima@igalia.com>
@@ -22,72 +22,54 @@
 #ifndef __EVD_TRANSPORT_H__
 #define __EVD_TRANSPORT_H__
 
-#include <evd-service.h>
+#include <glib-object.h>
+
 #include <evd-peer.h>
 
 G_BEGIN_DECLS
 
-typedef struct _EvdTransport EvdTransport;
-typedef struct _EvdTransportClass EvdTransportClass;
-typedef struct _EvdTransportPrivate EvdTransportPrivate;
+typedef struct _EvdTransport               EvdTransport;
+typedef struct _EvdTransportInterface      EvdTransportInterface;
 
-struct _EvdTransport
+struct _EvdTransportInterface
 {
-  EvdService parent;
-
-  EvdTransportPrivate *priv;
-};
-
-struct _EvdTransportClass
-{
-  EvdServiceClass parent_class;
+  GTypeInterface parent_iface;
 
   /* virtual methods */
-  EvdPeer * (* create_new_peer)   (EvdTransport *self);
-  gboolean  (* peer_is_connected) (EvdTransport *self,
-                                   EvdPeer      *peer);
-  gboolean  (* send)              (EvdTransport  *self,
-                                   EvdPeer       *peer,
-                                   const gchar   *buffer,
-                                   gsize          size,
-                                   GError       **error);
-  void      (* receive)           (EvdTransport *self,
-                                   EvdPeer      *peer,
-                                   gchar        *buffer,
-                                   gsize         size);
+  gssize   (* send)              (EvdTransport *self,
+                                  EvdPeer       *peer,
+                                  const gchar   *buffer,
+                                  gsize          size,
+                                  GError        **error);
+  void     (* receive)           (EvdTransport *self,
+                                  EvdPeer       *peer,
+                                  gchar         *buffer,
+                                  gsize          size);
+
+  gboolean (* peer_is_connected) (EvdTransport *self, EvdPeer *peer);
 
   /* signals */
   void (* signal_receive) (EvdTransport *self, gpointer user_data);
 };
 
-#define EVD_TYPE_TRANSPORT           (evd_transport_get_type ())
-#define EVD_TRANSPORT(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), EVD_TYPE_TRANSPORT, EvdTransport))
-#define EVD_TRANSPORT_CLASS(obj)     (G_TYPE_CHECK_CLASS_CAST ((obj), EVD_TYPE_TRANSPORT, EvdTransportClass))
-#define EVD_IS_TRANSPORT(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EVD_TYPE_TRANSPORT))
-#define EVD_IS_TRANSPORT_CLASS(obj)  (G_TYPE_CHECK_CLASS_TYPE ((obj), EVD_TYPE_TRANSPORT))
-#define EVD_TRANSPORT_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), EVD_TYPE_TRANSPORT, EvdTransportClass))
+#define EVD_TYPE_TRANSPORT                 (evd_transport_get_type ())
+#define EVD_TRANSPORT(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), EVD_TYPE_TRANSPORT, EvdTransport))
+#define EVD_IS_TRANSPORT(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EVD_TYPE_TRANSPORT))
+#define EVD_TRANSPORT_GET_INTERFACE(inst)  (G_TYPE_INSTANCE_GET_INTERFACE ((inst), EVD_TYPE_TRANSPORT, EvdTransportInterface))
 
+GType           evd_transport_get_type                      (void);
 
-GType             evd_transport_get_type                 (void) G_GNUC_CONST;
+gboolean        evd_transport_send                          (EvdTransport *self,
+                                                             EvdPeer       *peer,
+                                                             const gchar   *buffer,
+                                                             gsize          size,
+                                                             GError        **error);
+const gchar    *evd_transport_receive                       (EvdTransport *self,
+                                                             EvdPeer       *peer,
+                                                             gsize         *size);
 
-EvdTransport     *evd_transport_new                      (void);
-
-EvdPeer          *evd_transport_lookup_peer              (EvdTransport *self,
-                                                          const gchar  *id);
-
-GList            *evd_transport_get_all_peers            (EvdTransport *self);
-
-gboolean          evd_transport_send_utf8                (EvdTransport  *self,
-                                                          EvdPeer       *peer,
-                                                          const gchar   *str,
-                                                          GError       **error);
-
-gboolean          evd_transport_peer_is_dead             (EvdTransport *self,
-                                                          EvdPeer      *peer);
-
-const gchar      *evd_transport_receive                  (EvdTransport  *self,
-                                                          EvdPeer      **peer,
-                                                          gsize         *size);
+gboolean        evd_transport_peer_is_connected             (EvdTransport *self,
+                                                             EvdPeer       *peer);
 
 G_END_DECLS
 
