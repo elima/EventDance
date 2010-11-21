f42783a31d5198379cb4da4eac2b404e682dcb30
Eduardo Lima Mitev <elima@igalia.com>
2010-11-21 14:28:43 +0100

    Updates test-dbus-bridge to add new cases and sync with latest changes

--- tests/test-dbus-bridge.c
+++ tests/test-dbus-bridge.c
@@ -24,6 +24,9 @@
   "    <arg type=\\\"s\\\" name=\\\"greeting\\\" direction=\\\"in\\\"/>" \
   "    <arg type=\\\"s\\\" name=\\\"response\\\" direction=\\\"out\\\"/>" \
   "  </method>" \
+  "  <signal name=\\\"WorldGreets\\\">" \
+  "    <arg type=\\\"s\\\" name=\\\"message\\\"/>" \
+  "  </signal>" \
   "</interface>"
 
 static const gchar *session_bus_addr;
@@ -179,7 +182,7 @@ static const TestCase test_cases[] =
         NULL,
         "[9,3,1,'[\"/org/eventdance/lib/test/RegisterObject/Object\",\"" IFACE_XML "\"]']", /* register-object */
         "[9,4,1,'[\"/org/eventdance/lib/test/RegisterObject/Object\",\"" IFACE_XML "\"]']", /* register-object */
-        "[10,5,1,'[2]']", /* unregister-object */
+        "[10,5,1,'[1]']", /* unregister-object */
         "[6,6,1,'[1]']", /* unown-name */
       },
       {
@@ -193,26 +196,69 @@ static const TestCase test_cases[] =
       }
     },
 
-    { "new-proxy",
+    { "register-object/two-connections",
+      {
+        "[3,1,0,'[\"" DBUS_ADDR "\"]']", /* new-connection 1 */
+        "[3,2,0,'[\"" DBUS_ADDR "\"]']", /* new-connection 2 */
+        "[9,1,1,'[\"/org/eventdance/lib/test/RegisterObject/Object\",\"" IFACE_XML "\"]']", /* register-object, connection 1 */
+        "[9,1,2,'[\"/org/eventdance/lib/test/RegisterObject/Object\",\"" IFACE_XML "\"]']", /* register-object, connection 2 */
+        "[10,2,1,'[1]']", /* unregister-object, connection 1 */
+        "[10,2,2,'[2]']", /* unregister-object, connection 2 */
+      },
+      {
+        "[2,1,0,\"[1]\"]", /* new-connection 1 response */
+        "[2,2,0,\"[2]\"]", /* new-connection 2 response */
+        "[2,1,1,\"[1]\"]", /* register-object response, connection 1 */
+        "[2,1,2,\"[2]\"]", /* register-object response, connection 2 */
+        "[2,2,1,\"[]\"]", /* unregister-object response, connection 1 */
+        "[2,2,2,\"[]\"]", /* unregister-object response, connection 2 */
+      }
+    },
+
+    { "register-object/close-connection",
       {
         "[3,1,0,'[\"" DBUS_ADDR "\"]']", /* new-connection */
-        "[5,2,1,'[\"" BASE_NAME ".NewProxy\", 0]']", /* own-name */
+        "[5,2,1,'[\"org.eventdance.lib.tests.RegisterObject\", 0]']", /* own-name */
         NULL,
-        "[9,3,1,'[\"" BASE_OBJ_PATH "/NewProxy\",\"" IFACE_XML "\"]']", /* register-object */
-        "[11,4,1,'[\"" BASE_NAME "\",\"" BASE_OBJ_PATH "/NewProxy\",\"" BASE_IFACE_NAME ".TestIface\",0]']", /* new-proxy */
-        "[12,5,1,'[]']", /* close-proxy */
-        "[10,6,1,'[1]']", /* unregister-object */
-        "[6,7,1,'[1]']", /* unown-name */
+        "[9,3,1,'[\"/org/eventdance/lib/test/RegisterObject/Object\",\"" IFACE_XML "\"]']", /* register-object */
+        "[4,4,1,'[]']", /* close connection (should unregister object) */
+        "[10,5,1,'[0]']", /* unregister-object (should return error, invalid subject) */
       },
       {
         "[2,1,0,\"[1]\"]", /* new-connection response */
         "[2,2,1,\"[1]\"]", /* own-name response */
         "[7,0,1,\"[1]\"]", /* name-acquired signal */
         "[2,3,1,\"[1]\"]", /* register-object response */
-        "[2,4,1,\"[1]\"]", /* new-proxy response */
-        "[2,5,1,\"[]\"]", /* close-proxy response */
-        "[2,6,1,\"[]\"]", /* unregister-object response */
-        "[2,7,1,\"[]\"]", /* unown-name response */
+        "[2,4,1,\"[]\"]", /* close-connection response */
+        "[1,5,1,\"[3]\"]", /* error in unregister-object, invalid registered object */
+      }
+    },
+
+    { "new-proxy",
+      {
+        "[3,1,0,'[\"" DBUS_ADDR "\"]']", /* new-connection */
+        "[11,1,1,'[\"" BASE_NAME "\",\"" BASE_OBJ_PATH "/NewProxy\",\"" BASE_IFACE_NAME ".TestIface\",0]']", /* new-proxy */
+        "[12,1,1,'[]']", /* close-proxy */
+      },
+      {
+        "[2,1,0,\"[1]\"]", /* new-connection response */
+        "[2,1,1,\"[1]\"]", /* new-proxy response */
+        "[2,1,1,\"[]\"]", /* close-proxy response */
+      }
+    },
+
+    { "new-proxy/close-connection",
+      {
+        "[3,1,0,'[\"" DBUS_ADDR "\"]']", /* new-connection */
+        "[11,1,1,'[\"" BASE_NAME "\",\"" BASE_OBJ_PATH "/NewProxy\",\"" BASE_IFACE_NAME ".TestIface\",0]']", /* new-proxy */
+        "[4,2,1,'[]']", /* close connection (should invalidate proxy) */
+        "[12,1,1,'[]']", /* close-proxy */
+      },
+      {
+        "[2,1,0,\"[1]\"]", /* new-connection response */
+        "[2,1,1,\"[1]\"]", /* new-proxy response */
+        "[2,2,1,\"[]\"]", /* close-connection response */
+        "[1,1,1,\"[3]\"]", /* error in close-proxy, invalid proxy */
       }
     },
 
@@ -223,7 +269,7 @@ static const TestCase test_cases[] =
         NULL,
         "[9,3,1,'[\"" BASE_OBJ_PATH "/CallProxyMethod\",\"" IFACE_XML "\"]']", /* register-object */
         "[11,4,1,'[\"" BASE_NAME ".CallProxyMethod\",\"" BASE_OBJ_PATH "/CallProxyMethod\",\"" BASE_IFACE_NAME ".TestIface\",0]']", /* new-proxy */
-        "[13,5,1,'[\"HelloWorld\",\"[\\\"Hi there\\\"]\",\"(s)\",0,-1]']", /* call-method on proxy */
+        "[13,1,1,'[\"HelloWorld\",\"[\\\"Hi there\\\"]\",\"(s)\",0,-1]']", /* call-method on proxy */
         "[14,1,1,'[\"[\\\"hello world!\\\"]\",\"(s)\"]']", /* call-method-return from registered object */
       },
       {
@@ -232,11 +278,29 @@ static const TestCase test_cases[] =
         "[7,0,1,\"[1]\"]", /* name-acquired signal */
         "[2,3,1,\"[1]\"]", /* register-object response */
         "[2,4,1,\"[1]\"]", /* new-proxy response */
-        "[13,1,1,\"['HelloWorld','[ \\\"Hi there\\\" ]','(s)',0,0]\"]", /* call-method response on registered object */
-        "[14,5,1,\"['[ \\\"hello world!\\\" ]','(s)']\"]", /* call-method on proxy response */
+        "[13,1,1,\"['HelloWorld','[ \\\"Hi there\\\" ]','(s)',0,0]\"]", /* call-method to registered object */
+        "[14,1,1,\"['[ \\\"hello world!\\\" ]','(s)']\"]", /* call-method-return to proxy */
       }
-    }
+    },
 
+    { "proxy/signal",
+      {
+        "[3,1,0,'[\"" DBUS_ADDR "\"]']", /* new-connection */
+        "[5,2,1,'[\"" BASE_NAME ".ProxySignal\", 0]']", /* own-name */
+        NULL,
+        "[9,3,1,'[\"" BASE_OBJ_PATH "/ProxySignal\",\"" IFACE_XML "\"]']", /* register-object */
+        "[11,4,1,'[\"" BASE_NAME ".ProxySignal\",\"" BASE_OBJ_PATH "/ProxySignal\",\"" BASE_IFACE_NAME ".TestIface\",0]']", /* new-proxy */
+        "[15,1,1,'[\"WorldGreets\",\"[\\\"hello world!\\\"]\",\"(s)\"]']", /* emit-signal from registered object */
+      },
+      {
+        "[2,1,0,\"[1]\"]", /* new-connection response */
+        "[2,2,1,\"[1]\"]", /* own-name response */
+        "[7,0,1,\"[1]\"]", /* name-acquired signal */
+        "[2,3,1,\"[1]\"]", /* register-object response */
+        "[2,4,1,\"[1]\"]", /* new-proxy response */
+        "[15,0,1,\"['WorldGreets','[ \\\"hello world!\\\" ]','(s)']\"]", /* emit-signal received on proxy */
+      }
+    },
   };
 
 static void
