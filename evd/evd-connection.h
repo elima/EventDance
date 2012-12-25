a47ede11dc74fc6eb0332c6eaf9353e486b8bff2
Eduardo Lima Mitev <elima@igalia.com>
2012-12-25 20:03:51 +0100

    Fully removes the 'group' member from EvdConnection
    The group member has been moved to EvdIoStream.
    
    The 'group' property, the 'group-changed' signal, and the corresponding getter
    and setter methods were also removed.

--- evd/evd-connection.h
+++ evd/evd-connection.h
@@ -50,10 +50,6 @@ struct _EvdConnectionClass
 
   /* signal prototypes */
   void (* close)         (EvdConnection *self);
-  void (* group_changed) (EvdConnection    *self,
-                          EvdIoStreamGroup *new_group,
-                          EvdIoStreamGroup *old_group,
-                          gpointer          user_data);
   void (* write)         (EvdConnection *self);
 
   /* padding for future expansion */
@@ -102,10 +98,6 @@ gsize              evd_connection_get_max_writable     (EvdConnection *self);
 gboolean           evd_connection_is_connected         (EvdConnection *self);
 
 gint               evd_connection_get_priority         (EvdConnection *self);
-
-gboolean           evd_connection_set_group            (EvdConnection    *self,
-                                                        EvdIoStreamGroup *group);
-EvdIoStreamGroup  *evd_connection_get_group            (EvdConnection *self);
 
 void               evd_connection_lock_close           (EvdConnection *self);
 void               evd_connection_unlock_close         (EvdConnection *self);
