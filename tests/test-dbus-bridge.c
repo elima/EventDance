def18057912c406f32ba69c25ab33b2177cd986e
Eduardo Lima Mitev <elima@igalia.com>
2017-12-11 20:44:17 +0100

    tests: Updates expected output of a subcase in dbus-bridge test

--- tests/test-dbus-bridge.c
+++ tests/test-dbus-bridge.c
@@ -101,7 +101,7 @@ static const TestCase test_cases[] =
         "[3,1,0,0,'[\"invalid:address=error\",true]']",
       },
       {
-        "[1,1,0,0,\"[5,\\\"Unknown or unsupported transport 'invalid' for address 'invalid:address=error'\\\"]\"]"
+        "[1,1,0,0,\"[5,\\\"Unknown or unsupported transport \342\200\234invalid\342\200\235 for address \342\200\234invalid:address=error\342\200\235\\\"]\"]"
       }
     },
 
